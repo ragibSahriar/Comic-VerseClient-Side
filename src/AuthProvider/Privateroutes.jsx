35ac73655622b5b1d2bb2018a7ae4641b98d4dc1
seabeach11 <seebsk9@gmail.com>
2023-05-21 00:57:16 -0700

    added photo on navbar

--- src/AuthProvider/Privateroutes.jsx
+++ src/AuthProvider/Privateroutes.jsx
@@ -1,33 +1,13 @@
 import { useContext } from "react";
 import { AuthContext } from "./AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { ClockLoader } from "react-spinners";
 
 
 
 const Privateroutes = ({ children }) => {
   const { user} = useContext(AuthContext);
   const location = useLocation();
-
-//   if (loading) {
-//     return (
-//       <div className="mx-auto lg:my-48 lg:mx-96 sm:mt-9 align-middle justify-center">
-//         <Oval
-//           height={90}
-//           width={90}
-//           color="#000"
-//           wrapperStyle={{}}
-//           wrapperClass=""
-//           visible={true}
-//           ariaLabel="oval-loading"
-//           secondaryColor="#0002"
-//           strokeWidth={2}
-//           strokeWidthSecondary={2}
-//         />
-//         ;
-//       </div>
-//     );
-//   }
-
   if (user) {
     return children;
   }
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,57 +1,56 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
-// import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const {user, logOut} = useContext(AuthContext)
-    const handleLogOut = () => {
-        logOut()
-        .then()
-        .catch((err)=>{
-            console.log(err);
-        });
+  const { user, logOut } = useContext(AuthContext);
 
-    };
-    return (
-        <div className='flex justify-between'>
-            <div className="navbar bg-zinc-800 text-white font-semi-bold text-xl w-full flex justify-between">
-  <div className="">
-    <img className='w-16 rounded-xl' src="https://th.bing.com/th/id/OIP.V4n4RRWhkY_FcPr3ZJ9-CgHaHa?pid=ImgDet&rs=1" alt="" />
-  </div>
-  <div className="space-x-4 ">
-  <Link className='hover:text-black' to="/">Home</Link>
-  <Link className='hover:text-black' to="/alltoy">All Toys</Link>
-  {user && (
+  const handleLogOut = () => {
+    logOut()
+      .then()
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="flex justify-between">
+      <div className="navbar bg-zinc-800 text-white font-semi-bold text-xl w-full flex justify-between">
+        <div className="">
+          <img className="w-16 rounded-xl" src="https://th.bing.com/th/id/OIP.V4n4RRWhkY_FcPr3ZJ9-CgHaHa?pid=ImgDet&rs=1" alt="" />
+        </div>
+        <div className="space-x-4">
+          <Link className="hover:text-black" to="/">Home</Link>
+          <Link className="hover:text-black" to="/alltoy">All Toys</Link>
+          {user && (
             <Link className="hover:text-black" to="/mytoy">
               My Toys
             </Link>
           )}
-           {user && (
+          {user && (
             <Link className="hover:text-black" to="/addtoy">
               Add Toy
             </Link>
           )}
-  <Link className='hover:text-black' to="/blogs">Blogs</Link>
- 
-
-</div>
-  <div>
- {user? (
-            <li className="px-2 py-2 text-zinc-900 rounded bg-white text-xl font-bold list-none ">
-            <button onClick={handleLogOut}>Logout</button>
-          </li>
-        ) : (
-          <Link to="/login">
-            <li className="btn text-green bg-orange-700" >Login</li>
-          </Link>
-          
+          <Link className="hover:text-black" to="/blogs">Blogs</Link>
+        </div>
+        <div>
+          {user ? (
+            <div className="flex items-center">
+              {user.photoURL && (
+                <img className="w-12 h-12 mx-3 rounded-full" src={user.photoURL} alt="User Profile" />
+              )}
+              <button className="px-2 py-2 text-zinc-900 rounded bg-white text-xl font-bold list-none" onClick={handleLogOut}>Logout</button>
+            </div>
+          ) : (
+            <Link to="/login">
+              <li className="btn text-green bg-orange-700">Login</li>
+            </Link>
           )}
         </div>
-  </div>
-
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
